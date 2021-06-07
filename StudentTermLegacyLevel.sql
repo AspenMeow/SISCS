f3e19c8dead685d5b06a3b71c4763f3b5c4b2759
Di Chen <chendi4@opb.msu.edu>
2021-06-07 17:06:42 -0400

    fix milestone subquery

--- StudentTermLegacyLevel.sql
+++ StudentTermLegacyLevel.sql
@@ -5,6 +5,7 @@
 --Dependency table :  R_PRIMACY_RV need to be executed prior to this ---
 --add enrollment_career
 -- change population selection criteria, if none of the students plan is set to primary, the record is still bought in but with acad plan/prog, acad_level are set to blank
+-- fix problem with milestone subquery
 with a as 
  ((
  SELECT 
@@ -95,12 +96,11 @@ and P_STDNT_CAR_TERM.acad_career=P_STDNT_ENRL.acad_career
 and P_STDNT_CAR_TERM.institution=P_STDNT_ENRL.institution
 and P_STDNT_CAR_TERM.strm=P_STDNT_ENRL.strm)T)T
 WHERE T.RNUM=1
-and T.emplid in ('156333912','126863026','156297326','158749061','159690642','108417322','149697444','139244349','105044362','148452186','150387028')
---and T.emplid ='109035815'
+--and T.emplid in ('156333912','126863026','156297326','158749061','159690642','108417322','149697444','139244349','105044362','148452186','150387028')
+and T.emplid in ('101684982','102563917','101669215')
  ) 
  ),
  termlvlenrlprv as (
- 
  select a.emplid, a.strm,a.institution,a.acad_level_bot,a.term_descrshort, 
  (case when ordering=1 then 'E' when ordering =2 then 'W' 
  when ordering =3 then 'L' when ordering =4 then 'D' when ordering =5 then 'A' when ordering =6 then 'N' else '' end) as enrollment_status,
@@ -133,7 +133,7 @@ where planmax ='N'
 
 
 
-select ct.EMPLID,ct.STRM, ct.INSTITUTION, a.TERM_DESCRSHORT, ct.ACAD_CAREER, a.ENROLLMENT_STATUS as Enroll_Status,a.ACAD_PROG,a.ACAD_PLAN,  a.PRIMARY_CAR_FLAG,  a.PRIMARY_PLAN_FLAG,
+select ct.EMPLID,ct.STRM, ct.INSTITUTION, a.TERM_DESCRSHORT, ct.ACAD_CAREER, a.ENROLLMENT_STATUS ,a.ACAD_PROG,a.ACAD_PLAN,  a.PRIMARY_CAR_FLAG,  a.PRIMARY_PLAN_FLAG,
 ct.stdnt_car_nbr,ct.elig_to_enroll,
 ct.acad_level_bot,
 ct.acad_level_eot,
@@ -163,8 +163,7 @@ res.residency_dt  as residency_date,
 res.admission_res  as admission_res,
 res.tuition_res  as tuition_res,
 res.FIN_AID_FED_RES,
-(case when ct.ELIG_TO_ENROLL='Y' and ct.unt_passd_gpa>=12 and cur_gpa>=3.5 then 'Y' else 'N' end) as deans_list_flag,
---milestone COMP EXAM GRAD Only
+(case when ct.ELIG_TO_ENROLL='Y' and ct.unt_passd_gpa>=12 and ct.cur_gpa>=3.5 then 'Y' else 'N' end) as deans_list_flag,
 (case when ct.acad_career <> 'GRAD' or a.PRIMARY_CAR_FLAG ='N' then ' '
 when milestone.emplid is null then 'N' else 'Y' end ) as COMP_EXAM_COMPLETED,
 milestone.date_completed as COMP_EXAM_CMPL_DATE,
@@ -179,10 +178,10 @@ when adm.basis_admit_code='FY' then 'FRST'
 when adm.basis_admit_code='TR' then 'TRAN' 
 else ' ' end ) as Entry_Status_Code,
 (case when a.PRIMARY_CAR_FLAG='N' or a.PRIMARY_CAR_FLAG is null or First_Term_Career is null then ' '
-when First_Term_Career=a.strm and  adm.basis_admit_code='GR' and a.ACAD_CAREER='GRAD' and ct.ACAD_LEVEL_BOT = 'MAS' then 'Y'
+when First_Term_Career=a.strm  and a.ACAD_CAREER='GRAD' and ct.ACAD_LEVEL_BOT = 'MAS' then 'Y'
 else 'N' end) as FIRST_PRIM_GRAD_MASTERS ,
 (case when a.PRIMARY_CAR_FLAG='N' or a.PRIMARY_CAR_FLAG is null or First_Term_Career is null then ' '
-when First_Term_Career=a.strm and  adm.basis_admit_code='GR' and a.ACAD_CAREER='GRAD' and ct.ACAD_LEVEL_BOT = 'PHD' then 'Y'
+when First_Term_Career=a.strm  and a.ACAD_CAREER='GRAD' and ct.ACAD_LEVEL_BOT = 'PHD' then 'Y'
 else 'N' end) as First_Prim_GRAD_Doctoral  ,
 (case when a.PRIMARY_CAR_FLAG='N' or a.PRIMARY_CAR_FLAG is null or First_Term_Career is null then ' '
 when First_Term_Career=a.strm and d.Enrld_MSU_Prior='Y' then 'CONT'
@@ -205,7 +204,7 @@ when First_Term_Career=a.strm then 'NEW'
 when d.MSU_Atnd_Preced_Flag='Y' then 'RTRN'
 else 'RGAP' end ) in ('NEW','CONT') )then 'Y'
 else 'N' end ) as Fall_Entering_Cohort,
-
+acdstd.ACAD_STNDNG_ACTN ,acdstd.ACAD_STNDNG_STAT,ct.TOT_PASSD_NOGPA, ct.TOT_TAKEN_NOGPA, ct.UNT_PASSD_NOGPA, ct.UNT_TAKEN_NOGPA ,
 (case when greatest(nvl(exawd.postdeg,0),nvl(intawd.postdeg,0))=1 then '06'
 when greatest(nvl(exawd.postdeg,0),nvl(intawd.postdeg,0))=2 then '10'
 when greatest(nvl(exawd.postdeg,0),nvl(intawd.postdeg,0))=3 then '13'
@@ -216,7 +215,8 @@ when greatest(nvl(exawd.postdeg,0),nvl(intawd.postdeg,0))=7 then '18'
 when greatest(nvl(exawd.postdeg,0),nvl(intawd.postdeg,0))=8 then '15'
 when greatest(nvl(exawd.postdeg,0),nvl(intawd.postdeg,0))=9 then '21'
 else ' '
-end)  Prior_High_Degree ,acdstd.ACAD_STNDNG_ACTN ,acdstd.ACAD_STNDNG_STAT,
+end)  Prior_High_Degree ,
+--,acdstd.ACAD_STNDNG_ACTN ,acdstd.ACAD_STNDNG_STAT,
 a.STUDENT_LEVEL as RPT_DEGREE_CAREER,
 (case when a.PRIMARY_CAR_FLAG='N' or a.PRIMARY_CAR_FLAG is null or First_Term_Rpt_Level is null then ' ' 
 when  First_Term_Rpt_Level=a.strm then 'Y' else 'N' end ) as RPT_FIRST_PRIM_DEGR_CAR,
@@ -240,7 +240,11 @@ else 'RGAP' end ) in ('NEW','CONT') )then 'Y'
 else 'N' end ) as RPT_IPEDS_FALL_COHORT,
 a.enrollment_career
 from termlvlenrl a
---first term at lvl
+inner join SISCS.P_STDNT_CAR_TERM_V ct
+on a.emplid=ct.emplid
+and a.strm=ct.strm
+and a.institution=ct.institution
+and a.acad_career=ct.acad_career 
 left join 
    ( select emplid, institution, student_level,strm,
             COUNT(*) OVER(PARTITION BY emplid, institution, student_level
@@ -253,7 +257,7 @@ left join
                         )   as First_Term_Rpt_Level
         from (select * from termlvlenrl 
             where  PRIMARY_CAR_FLAG='Y' and primary_plan_flag='Y' and enrollment_status in ('E','W'))
-        ) b
+        ) b --make b a logic view itself
 on a.emplid=b.emplid 
 and a.institution=b.institution
 and  b.cnt=1
@@ -271,13 +275,11 @@ left join
                         )   as First_Term_Career
         from (select distinct emplid, institution,ACAD_CAREER,strm  from termlvlenrl 
             where  PRIMARY_CAR_FLAG='Y' and primary_plan_flag='Y' and enrollment_status in ('E','W')
-    )) c
+    )) c -- make c a logic view itself
 on a.emplid=c.emplid 
 and a.institution=c.institution
 and  c.cnt=1
 and a.acad_career=c.acad_career
----MSU_Atnd_Preced_Flag
---Enrld_MSU_Prior
 left join (
         select emplid, strm, descrshort, MSU_Atnd_Preced_Flag,
         (case when LAG(enrlincludcur, 1, '') OVER(PARTITION BY emplid  ORDER BY emplid, strm ) is  null then 'N'
@@ -305,10 +307,9 @@ left join (
                 on b.emplid=tm.emplid and a.strm=tm.strm and tm.enrollment_status in ('E','W')
                 where a.EDW_ACTV_IND='Y'
                 ))
-        ) d
+        ) d --make subquery d a logical view itself
 on  a.emplid= d.emplid
 and a.strm=d.strm
---cohort entry based on career
 left join (select a.*, b.acad_year as cohort_entry
             from (
                     select  emplid, institution, acad_career,
@@ -325,12 +326,11 @@ left join (select a.*, b.acad_year as cohort_entry
     and a.acad_career=b.acad_career
     and a.minstrm=b.strm
     where b.EDW_ACTV_IND='Y'
-) entrycohort
+) entrycohort -- make entry cohort a logical view itself
 on a.emplid=entrycohort.emplid
 and a.institution=entrycohort.institution
 and a.acad_career=entrycohort.acad_career
 and  (case when a.acad_career='GRAD' then a.acad_level_bot else a.acad_career end )= entrycohort.acad_gr_lvl
---cohort entry based on lvl
 left join (select a.*, b.acad_year as cohort_entry_lvl
             from (
                     select  emplid, institution, acad_career,student_level,
@@ -347,7 +347,7 @@ left join (select a.*, b.acad_year as cohort_entry_lvl
     and a.acad_career=b.acad_career
     and a.minstrm=b.strm
     where b.EDW_ACTV_IND='Y'
-) entrycohortlvl
+) entrycohortlvl ----make subquery entrycohort lvl a logical view itself
 on a.emplid=entrycohortlvl.emplid
 and a.institution=entrycohortlvl.institution
 and a.student_level=entrycohortlvl.student_level
@@ -363,7 +363,7 @@ on a.emplid=firstplan.emplid
 and a.institution= firstplan.institution
 and a.acad_career= firstplan.acad_career
 and a.ACAD_PLAN=firstplan.acad_plan
---highest prior degree
+ 
 left join 
 (
 select a.emplid,  a.strm, 
@@ -392,14 +392,12 @@ and a.EDW_ACTV_IND='Y'
 and t.EDW_ACTV_IND='Y'
 and b.EDW_ACTV_IND='Y'
 and d.EDW_ACTV_IND='Y'
-group by a.emplid, a.strm ) exawd
+group by a.emplid, a.strm ) exawd --make exawd a logic view itself
 on a.emplid= exawd.emplid
 and a.strm= exawd.strm
---higest internal degree
 left join (select ct.emplid, ct.strm, max(dg.postdeg) as postdeg
 from   SISCS.P_STDNT_CAR_TERM_V ct
 inner join (
-
 select a.emplid,  (case when d.education_lvl='06' then 1
 when d.education_lvl='10' then 2
 when d.education_lvl='13' then 3
@@ -425,15 +423,13 @@ when d.education_lvl='19' then 5
 when d.education_lvl='17' then 6
 when d.education_lvl='18' then 7
 when d.education_lvl='15' then 8
-when d.education_lvl='21' then 9 else 0 end )) dg
+when d.education_lvl='21' then 9 else 0 end )) dg --make subquery dg a logic view itself
 on ct.emplid=dg.emplid
 and ct.strm> dg.minterm
 where ct.EDW_ACTV_IND='Y'
 group by ct.emplid, ct.strm) intawd
 on a.emplid= intawd.emplid
 and a.strm= intawd.strm
-
---honors Honors_College_Member
 left join (
             select g.emplid,scar.strm,t.descrshort,g.institution, g.stdnt_group,scar.acad_career
             from siscs.p_stdnt_grps_hist_v g,
@@ -462,7 +458,7 @@ left join (
             and  g.stdnt_group='XHON'
             and g.EDW_ACTV_IND='Y' 
             and scar.EDW_ACTV_IND='Y'
-            and t.EDW_ACTV_IND='Y') honr
+            and t.EDW_ACTV_IND='Y') honr --make honr a logical view itself
 on a.emplid=honr.emplid
 and a.acad_career= honr.acad_career
 and a.institution=honr.institution
@@ -489,18 +485,17 @@ and res.effective_term= (
         and r.EDW_ACTV_IND='Y'
       and ct.strm >= r.effective_term
 
-) where  res.EDW_ACTV_IND='Y' ) res
+) where  res.EDW_ACTV_IND='Y' ) res --make res a logic view itself
 on ct.emplid =res.emplid
 and ct.acad_career=res.acad_career
 and ct.strm= res.strm
---add comp exam milestones for grad
-left join ( select a.emplid, a.institution,a.acad_career,a.acad_plan_acad_prog,ms.milestone, ms.date_completed, ms.MILESTONE_COMPLETE, a.strm
-             from SISCS.R_PRIMACY_RV a
+left join ( select a.emplid, a.institution,a.acad_career,a.acad_prog,ms.milestone, ms.date_completed, ms.MILESTONE_COMPLETE, a.strm
+             from termlvlenrl a
              left join siscs.p_stdnt_car_mlstn_v ms
             on a.emplid=ms.emplid
             and a.institution=ms.institution
             and a.acad_career=ms.acad_career
-            and a.acad_plan_acad_prog=ms.acad_prog
+            and a.acad_prog=ms.acad_prog
             and a.primary_plan_flag='Y'
             and ms.effdt = (select max(effdt)
                             from siscs.p_stdnt_car_mlstn_v ms1
@@ -510,21 +505,31 @@ left join ( select a.emplid, a.institution,a.acad_career,a.acad_plan_acad_prog,m
                             and ms.acad_prog=ms1.acad_prog
                             and ms1.EDW_ACTV_IND='Y'
                             )
+                            
+            and ms.milestone_nbr = (select max(milestone_nbr)
+                            from siscs.p_stdnt_car_mlstn_v ms2
+                            where ms.emplid=ms2.emplid
+                            and ms.institution=ms2.institution
+                            and ms.acad_career=ms2.acad_career
+                            and ms.acad_prog=ms2.acad_prog
+                            and ms.effdt = ms2.effdt
+                            and ms2.EDW_ACTV_IND='Y'
+                            )                
             and ms.EDW_ACTV_IND='Y'
-) milestone
+) milestone --make milestone a logical view itself
 on a.emplid=milestone.emplid
 and a.institution=milestone.institution
 and a.strm=milestone.strm
 and a.acad_career=milestone.acad_career
-and a.acad_prog = milestone.acad_plan_acad_prog
+and a.acad_prog = milestone.acad_prog
 and milestone.milestone='GCOMPEXAM'
 and milestone.ACAD_CAREER = 'GRAD' 
 and milestone.MILESTONE_COMPLETE = 'Y'
+---and ct.stdnt_car_nbr=milestone.stdnt_car_nbr
 left join siscs.p_adm_basis_admit_v adm
 on a.emplid= adm.emplid
 and a.institution= adm.institution
 and a.acad_career= adm.acad_career
--- add academic standing end term
 left join (
 select acadstdng1.emplid, acadstdng1.acad_career, acadstdng1.institution, acadstdng1.strm, effdt,effseq,ACAD_STNDNG_ACTN ,ACAD_STNDNG_STAT ,
 COUNT(*) OVER(
